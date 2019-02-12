058627a3723b97c1ecc3f0c763caba2f808c6be8
vick3d <viktoro0090@gmail.com>
2019-02-12 16:19:45 +0100

    add test for visit the page, passing

---
+++ features/step_definitions/landing_page_steps.rb
@@ -0,0 +1,12 @@
+When("I visit the page") do
+    root_path '/'
+  end
+  
+  Then("I should see Sign-up") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("Then I should be able to click on {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
---
+++ features/visitor_is_able_to_sign_up.feature
@@ -0,0 +1,12 @@
+Feature: Visitor is able to sign up
+    As a User
+    In order to access the mailboxer
+    I want to be able to sign up
+
+    Scenario:
+        When I visit the page
+        Then I should see Sign-up
+        And Then I should be able to click on "Sign up"
+        
+
+
