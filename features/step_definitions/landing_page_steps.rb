e038f7f6c745ff82e96d9d5948478eacb9cb27d4
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-02-13 10:49:55 +0100

    adds feature testing for click on create and signed up successfully message

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,23 +1,19 @@
 Given ("I visit the page") do
-    visit root_path 
-  end
-  
-  When("I click on {string}") do |button|
-    click_on button
-  end
-  
-  Then("I should be redirected to sign up page") do
-    expect(current_path).to eq new_user_registration_path
-  end
-  
-  Then("I fill in {string} with {string}") do |field, content|
-    fill_in field, with: content
-  end
-  
-  When("I click {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then("I should see {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+	visit root_path
+end
+
+When("I click on {string}") do |button|
+	click_on button
+end
+
+Then("I should be redirected to sign up page") do
+	expect(current_path).to eq new_user_registration_path
+end
+
+Then("I fill in {string} with {string}") do |field, content|
+	fill_in field, with: content
+end
+
+Then("I should see {string}") do |string|
+	expect(string).to eq "Welcome! You have signed up successfully."
+end
--- features/visitor_is_able_to_sign_up.feature
+++ features/visitor_is_able_to_sign_up.feature
@@ -5,15 +5,11 @@ Feature: Visitor is able to sign up
 
     Scenario:
         Given I visit the page
-        When I click on "Sign up" 
+        When I click on "Sign up"
         Then I should be redirected to sign up page
-        Then I fill in "Name" with "Boa" 
-        And I fill in "Email" with "dopekids@live.com" 
+        Then I fill in "Name" with "Boa"
+        And I fill in "Email" with "dopekids@live.com"
         And I fill in "Password" with "Wrap009Do"
         And I fill in "Password confirmation" with "Wrap009Do"
-        When I click "Create" 
+        When I click on "Create"
         Then I should see "Welcome! You have signed up successfully."
-
-        
-
-
