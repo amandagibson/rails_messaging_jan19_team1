a7b7c2f9444dbca504766e9fc3a04379a58c8b63
vick3d <viktoro0090@gmail.com>
2019-02-14 23:40:47 +0100

    adds ':user' instead of 'user'

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -19,7 +19,7 @@
 
   Given("I am logged in as {string}") do |name|
     user = User.find_by(name: name)
-    login_as user, scope: user
+    login_as user, scope: :user
   end
   
   Given("I am visiting the {string}") do |string|
