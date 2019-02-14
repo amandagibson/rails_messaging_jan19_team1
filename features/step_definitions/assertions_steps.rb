4c268f3bb4877883c9acf39d716f50a5ab7b13f8
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-02-14 16:47:59 +0100

    removes an asertion

--- features/step_definitions/assertions_steps.rb
+++ features/step_definitions/assertions_steps.rb
@@ -2,10 +2,6 @@ Then("I should see {string}") do |content|
 	expect(page).to have_content content
 end
 
-Then("I should be redirected to sign up page") do
-	expect(current_path).to eq new_user_registration_path
-end
-
 Then("I am logged into my account") do
 	expect(current_path).to eq welcome_index
 end
